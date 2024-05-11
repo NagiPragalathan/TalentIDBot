d4b87fcea6d4601df69fef2490d2aa8531a1c3a3
NagiPragalathan <nagipragalathan@gmail.com>
2024-05-12 00:58:58 +0530

    messages are added

--- express-vercel/index.js
+++ express-vercel/index.js
@@ -1,6 +1,15 @@
 const express = require("express");
 const app = express();
 const product = require("./api/product");
+const TelegramBot = require('node-telegram-bot-api');
+
+
+const TOKEN = "6641998982:AAHVM_Gds40IaEaFIKuEXwsIwD53bz9-834"
+const bot = new TelegramBot(TOKEN, { polling: true });
+bot.on('message', (msg) => {
+    const chatId = msg.chat.id;
+    bot.sendMessage(chatId, 'Received your message: ' + msg.text);
+});
 
 app.use(express.json({ extended: false }));
 
